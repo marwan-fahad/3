d3ec2fbc95e0c405a679639e81f9df4d07fec884
marwan-fahad <121671118+marwan-fahad@users.noreply.github.com>
2023-01-05 18:15:04 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var weight = [35,38,42,45,43,34,36,41,48,32];
+var sum = 0;
+var average;
+function setup() {
+  createCanvas(400,400);
+for(var i = 0;i<weight.length;i++){
+sum= sum+weight[i];
+average = sum/weight.length;
+}
+console.log(average);
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
